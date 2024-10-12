253f81a9f8a56b8ee26b9634fbafeb4dfd0b735c
Faizan Ahmad <faizankhan56752@gmail.com>
2024-10-13 01:13:01 +0530

    Added half landing page

---
+++ src/app/components/Phone.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils"
+import { HTMLAttributes } from "react"
+
+interface phoneProps extends HTMLAttributes<HTMLDivElement> {
+    dark?: boolean,
+    imgSrc: string
+}
+
+const Phone = ({ imgSrc, dark = false, className, ...props }: phoneProps) => {
+    return (
+        <div className={cn("relative pointer-events-none overflow-hidden z-50", className)} {...props}>
+
+            <img src="/phone-template-white-edges.png" alt="" className="select-none z-50 pointer-events-auto" />
+
+            <div className="absolute -z-10 inset-0">
+                <img src={imgSrc} alt="" className="object-cover" />
+            </div>
+        </div>
+    )
+}
+
+export default Phone
